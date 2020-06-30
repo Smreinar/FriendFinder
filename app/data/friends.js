38763caf27a4f3d2641c4ee4d2b9903a42957abb
steven Reinart <stevenreinart123@gmail.com>
2020-06-30 14:17:07 -0700

    added an array of objects in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+const friendsData = [
+    {
+      name: "Butters",
+      photo: "",
+      scores: [1, 2, 4, 2, 3, 4, 2, 5, 4, 3]
+    },
+    {
+      name: "Grinch",
+      photo:"",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Mario",
+      photo:"",
+      scores: [4, 3, 4, 5, 4, 5, 4, 5, 4, 5]
+    },
+    {
+      name: "Chowder",
+      photo:"",
+      scores: [2, 2, 2, 4, 4, 1, 3, 3, 3, 3]
+    },
+    {
+      name: "Boo",
+      photo:"",
+      scores: [1, 1, 2, 3, 2, 1, 2, 1, 2, 1]
+    },
+    {
+      name: "Bowser",
+      photo:"",
+      scores: [2, 1, 2, 1, 1, 2, 1, 2, 1, 2]
+    },
+    {
+      name: "Princess Peach",
+      photo:"",
+      scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
