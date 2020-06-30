3417f02fbe665805b199a23bacdee485d5402688
smreinar <stevenreinart123@gmail.com>
2020-06-30 09:50:01 -0700

    file structure +survey.html with Questions

---
+++ server.js
@@ -0,0 +1,18 @@
+//Dependences
+
+var express = require("express");
+var path = require("path");
+// var PORT = process.env.PORT || 3000;
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
