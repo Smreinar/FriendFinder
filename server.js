6b584bce711fcf05d5f22d5b2579762a2e6bea5a
steven Reinart <stevenreinart123@gmail.com>
2020-06-30 13:35:50 -0700

    finished the htmlRoutes and got it to interact with the server

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 //Dependences
 
 var express = require("express");
-var path = require("path");
+
 // var PORT = process.env.PORT || 3000;
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
+
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// const apiRoutes = require('./app/routing/apiRoutes');
+const htmlRoutes = require('./app/routing/htmlRoutes');
+
+// apiRoutes(app)
+htmlRoutes(app)
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
