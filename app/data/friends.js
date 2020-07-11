d286e7c9dd8cc09f9a7b1cd188dcad87e3d47f9a
steven Reinart <stevenreinart123@gmail.com>
2020-07-11 11:33:01 -0700

    added Images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,37 @@
 const friendsData = [
     {
       name: "Butters",
-      photo: "",
+      photo: "https://southparkstudios.mtvnimages.com/shared/characters/kids/butters-stotch.png",
       scores: [1, 2, 4, 2, 3, 4, 2, 5, 4, 3]
     },
     {
       name: "Grinch",
-      photo:"",
+      photo:"https://i.pinimg.com/originals/58/2d/34/582d3421899f539df9bcc759c9a0e0f9.jpg",
       scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     },
     {
       name: "Mario",
-      photo:"",
+      photo:"https://mario.nintendo.com/assets/img/home/char-mario.png",
       scores: [4, 3, 4, 5, 4, 5, 4, 5, 4, 5]
     },
     {
       name: "Chowder",
-      photo:"",
+      photo:"https://vignette.wikia.nocookie.net/chowder/images/3/3f/Chowder-4.png/revision/latest/scale-to-width-down/340?cb=20190118000559",
       scores: [2, 2, 2, 4, 4, 1, 3, 3, 3, 3]
     },
     {
       name: "Boo",
-      photo:"",
+      photo:"https://www.mariowiki.com/images/a/a1/Boo_CTTT.png",
       scores: [1, 1, 2, 3, 2, 1, 2, 1, 2, 1]
     },
     {
       name: "Bowser",
-      photo:"",
+      photo:"https://miro.medium.com/max/6064/1*x2Gw1gKrRQi5WEcBrKYA1g.png",
       scores: [2, 1, 2, 1, 1, 2, 1, 2, 1, 2]
     },
     {
       name: "Princess Peach",
-      photo:"",
+      photo:"https://www.mariowiki.com/images/thumb/b/b7/SuperMarioParty_Peach_2.png/1200px-SuperMarioParty_Peach_2.png",
       scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     }
   ];
